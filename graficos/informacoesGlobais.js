972c9a2e84d494251254b3722e51ac8cedd8363f
gabrieledeidara <114776941+gabrieledeidara@users.noreply.github.com>
2024-10-16 12:04:23 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -2,7 +2,7 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
 async function visualizarInformacoesGlobais() {
-    const res = await fetch(url)
+    const res = await fetch(url);
     const dados = await res.json()
     
     const paragrafo = document.createElement('p')
