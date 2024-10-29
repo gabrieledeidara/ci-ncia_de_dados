143540c52a2ebe6e328693a7cc9790227eefc3df
gabrieledeidara <114776941+gabrieledeidara@users.noreply.github.com>
2024-10-29 07:57:53 -0300

    ok

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -2,4 +2,6 @@ async function quantidadeUsuarios() {
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/mais/numero-usuarios.json'
 const res = await fetch (url)
 const dados = await res.json()    
+console.log(dados)
 }
+quantidadeUsuarios()
