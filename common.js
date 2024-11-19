cb9de56073fd465dbf399b3d07c8293502b942dc
gabrieledeidara <114776941+gabrieledeidara@users.noreply.github.com>
2024-11-19 07:49:34 -0300

    Update common.js

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ const getCSS = (variavel) => {
   return getComputedStyle(document.body).getPropertyValue(variavel)
 };
 const tickConfig = {
-  color: getCss('--primary-color'),
+  color: getCSS('--primary-color'),
   size:15,
   family: getCSS('--font')
 }
