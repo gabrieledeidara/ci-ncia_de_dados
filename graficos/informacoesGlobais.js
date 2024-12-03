9401929d2780abc15828d47eae0082eae92b2fad
gabrieledeidara <114776941+gabrieledeidara@users.noreply.github.com>
2024-12-03 07:41:02 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -11,7 +11,9 @@ async function vizualizarInformacoesGlobais() {
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente 
+    span>${pessoasEstudando} bilhões</span> estão estuano passam e pasam  em média <span>${horas} horas</span> e <span>${minutos} 
+    minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemEstudando}%</span> de pessoas estão matriculadas em alguma ecola.`
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
